440bbe5d0ac33de8e3c271955e2d50aefbd07ab6
Connor Zufelt <cjzufelt@gmail.com>
2020-03-24 03:56:56 +0000

    Add server.js, run by node server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public'));
+
+app.listen(3001, () => console.log('Server listening on port 3001!'));
